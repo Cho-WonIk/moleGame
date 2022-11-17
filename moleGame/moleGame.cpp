9fc58781669074b2a48c627ef216a903939ecdf5
cwi0212 <cwi0212@naver.com>
2022-11-17 13:02:19 +0900

    초기 커밋.

---
+++ moleGame/moleGame.cpp
@@ -0,0 +1,27 @@
+#include "GL/glut.h"
+
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin
+	(GL_POLYGON);
+	glVertex2f(-0.2f, -0.2f);
+	glVertex2f(0.4f, -0.4f);
+	glVertex2f(0.4f, 0.4f);
+	glVertex2f(-0.2f, 0.2f);
+
+	glEnd();
+	glFinish();
+}
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv); // initilize 초기화
+	glutCreateWindow
+	("OpenGL");
+
+	glutDisplayFunc(display); // display이벤트 발생
+	glutMainLoop();
+	return 0;
+}
