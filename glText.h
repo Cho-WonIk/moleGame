55d047cf00d51800b2a7056817606cbf67f480fa
cwi0212 <cwi0212@naver.com>
2022-11-22 12:09:53 +0900

    .

--- glText.h
+++ glText.h
@@ -2,8 +2,8 @@
 #include <windows.h> // Header File For Windows
 #include <stdio.h>   // Header File For Standard Input/Output
 #include <stdarg.h>  // Header File For Variable Argument Routines
-#include <gl\gl.h>   // Header File For The OpenGL32 Library
-#include <gl\glu.h>  // Header File For The GLu32 Library
+#include <GL/GL.h>   // Header File For The OpenGL32 Library
+#include <GL/GLU.h>  // Header File For The GLu32 Library
 
 enum FONT
 {
