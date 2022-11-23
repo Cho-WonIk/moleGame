5fdadf44a724b2a67be0d5998a0c113fddc539a1
cwi0212 <cwi0212@naver.com>
2022-11-23 10:06:48 +0900

    스코어표시

--- glText.cpp
+++ glText.cpp
@@ -1,11 +1,11 @@
 #include "glText.h"
 
-void glBitmapText(float x, float y, float z, char* string, void* font)
+void glBitmapText(float x, float y, float z, std::string text, void* font)
 {
-    char* c;
     glRasterPos3f(x, y, z);
-    for (c = string; *c != '\0'; c++)
-    {
-        glutBitmapCharacter(font, *c);
-    }
+	for (int i = 0; i < text.length(); i++)
+	{
+		char x = text.at(i);
+		glutBitmapCharacter(font, (int)x);
+	}
 }
--- glText.h
+++ glText.h
@@ -2,5 +2,6 @@
 #include <GL/glut.h>
 #include <GL/GL.h>
 #include <GL/GLU.h>
+#include <string>
 
-void glBitmapText(float x, float y, float z, char* string, void* font = GLUT_BITMAP_HELVETICA_18);
+void glBitmapText(float x, float y, float z, std::string text, void* font = GLUT_BITMAP_HELVETICA_18);
